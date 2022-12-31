040b157b7f0d7265acd11d3b61dbdb0d2e40035f
yaroslavvasilenko <yaroslav.vasilenko3101@gmail.com>
2022-12-31 08:07:40 +0300

    !

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Panic(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hi"))
+}
